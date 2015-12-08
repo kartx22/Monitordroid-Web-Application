7b3495a548d7ada61368d1dcdf2ae8a4c3d689fe
kartx22 <kartx.rockeveryone@gmail.com>
2015-12-08 13:19:44 +0530

    Update viewfilesdata.php

--- src/viewfilesdata.php
+++ src/viewfilesdata.php
@@ -12,7 +12,7 @@
     $rowId = $_POST["rowid"];
     $reqTime = intval($_POST["reqtime"]);
 
-	include_once './db_functions.php';
+	include_once '../db_functions.php';
 	$db = new DB_Functions();
 
     if (! $db->userDeviceVerify($rowId, $email)) {
