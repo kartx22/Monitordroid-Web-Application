483eb18be513f51f48f15efd8bed9cc5a2864562
kartx22 <kartx.rockeveryone@gmail.com>
2015-12-08 12:29:04 +0530

    Update applistdata.php

--- src/applistdata.php
+++ src/applistdata.php
@@ -27,7 +27,7 @@
     $rowId = $_POST["rowid"];
     $reqTime = intval($_POST["reqtime"]);
 
-    include_once './db_functions.php';
+    include_once '../db_functions.php';
 
     $db = new DB_Functions();
 
