d534e422d241e7bd0592b57e64fae79147d625ea
kartx22 <kartx.rockeveryone@gmail.com>
2015-12-08 12:41:39 +0530

    Update contactsdata_np.php

--- src/contactsdata_np.php
+++ src/contactsdata_np.php
@@ -10,7 +10,7 @@ if(!isset($_SESSION)) session_start();
 	$rName=$_SESSION["registration"];
 	$rowId=$_SESSION["rowid"];
 
-	include_once './db_functions.php';
+	include_once '../db_functions.php';
 
 	$db = new DB_Functions();
 	$contactslist = $db->getContacts($rName);
