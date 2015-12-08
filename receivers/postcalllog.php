a6876d93f602a8d5dc3174c69d3b412a8b220929
kartx22 <kartx.rockeveryone@gmail.com>
2015-12-08 13:26:26 +0530

    Update postcalllog.php

--- receivers/postcalllog.php
+++ receivers/postcalllog.php
@@ -3,7 +3,7 @@
     $data= $_POST["LogData"];
 	$rName=$_POST["regName"];
 	
-	include_once './db_functions.php';
+	include_once '../db_functions.php';
 	$db = new DB_Functions();
 	$db->insertCallLogs($rName, $data);
     ?>
