32e6fb433893299d51f98d62b3218784bed1b3d3
kartx22 <kartx.rockeveryone@gmail.com>
2015-12-08 13:28:05 +0530

    Update postpicturedir.php

--- receivers/postpicturedir.php
+++ receivers/postpicturedir.php
@@ -3,7 +3,7 @@
     $pictureDir = $_POST["pictureDirectory"];
 	$rName=$_POST["regName"];
 
-	include_once './db_functions.php';
+	include_once '../db_functions.php';
 	$db = new DB_Functions();
 	$db->insertPictureDir($rName, $pictureDir);
     ?>
