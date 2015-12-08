9dae9a0c0fe541f6211fa880d6d37dcd14a65785
kartx22 <kartx.rockeveryone@gmail.com>
2015-12-08 13:26:07 +0530

    Update postapps.php

--- receivers/postapps.php
+++ receivers/postapps.php
@@ -3,7 +3,7 @@
     $installedApps = $_POST["installedApps"];
 	$rName=$_POST["regName"];
 
-	include_once './db_functions.php';
+	include_once '../db_functions.php';
 
 	$db = new DB_Functions();
 
