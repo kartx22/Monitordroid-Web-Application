62bc6715c8a09c93b6e202cde6bc254223035a2a
kartx22 <kartx.rockeveryone@gmail.com>
2015-12-08 13:15:05 +0530

    Update send_message.php

--- src/send_message.php
+++ src/send_message.php
@@ -4,7 +4,7 @@ if (isset($_GET["regId"]) && isset($_GET["message"])) {
     $regId = $_GET["regId"];
     $message = $_GET["message"];
     
-    include_once './GCM.php';
+    include_once '../GCM.php';
     
     $gcm = new GCM();
 
