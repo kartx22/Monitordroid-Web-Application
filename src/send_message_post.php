e0a57cef854014221c80ab7b24980fab8a6f4ed3
kartx22 <kartx.rockeveryone@gmail.com>
2015-12-08 13:16:11 +0530

    Update send_message_post.php

--- src/send_message_post.php
+++ src/send_message_post.php
@@ -3,7 +3,7 @@ if (isset($_POST["regId"]) && isset($_POST["message"])) {
     $regId = $_POST["regId"];
     $message = $_POST["message"];
     
-    include_once './GCM.php';
+    include_once '../GCM.php';
     
     $gcm = new GCM();
 
