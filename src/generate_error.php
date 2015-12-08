8a7994c2082aa981a59ae03e6ba24b8e49ea1b2e
kartx22 <kartx.rockeveryone@gmail.com>
2015-12-08 12:54:36 +0530

    Update generate_error.php

--- src/generate_error.php
+++ src/generate_error.php
@@ -2,6 +2,6 @@
 if (!isset($_SESSION["email"])) {
     die();
 }
-define("LOG_FILE", "./jserrors.log");
+define("LOG_FILE", "../jserrors.log");
 error_log(date('[Y-m-d H:i] '). ": [" . $_SESSION["email"] . "] ERR: " . $_POST["msg"] . " @ " . $_POST["url"] . ":" . $_POST["linenumber"] . ":" . $_POST["column"] . " (" . $_POST["errorObj"] . ")" . PHP_EOL, 3, LOG_FILE);
 ?>
